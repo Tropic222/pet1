03b767c99512e531f083c73f34645cb9e6e0244b
Tropic222 <deputat1120@gmail.com>
2024-09-28 15:19:39 +0300

    displaying information about the city in tab_01

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 window.location.href = "#tab_01";
 
-const cityName = "London"; 
-const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+const weatherForm = document.getElementById('weatherForm');
+const nowTab = document.querySelector('#tab_01');
+const input = document.getElementById('input');
+const iconElement = document.querySelector('.weather__block-icon');
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Ошибка:', error));
+weatherForm.addEventListener('submit', getData);
+
+function getData(event) {
+  event.preventDefault(); /
+  const cityName = input.value;
+  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const temperature = Math.round(data.main.temp);
+      const cityName = data.name;
+      const aicon = data.weather[0].icon;
+
+      
+      nowTab.querySelector('.weather__block-temp').textContent = `${temperature}°`;
+      nowTab.querySelector('.weather__block-content-city').textContent = cityName;
+      iconElement.setAttribute('src', `http://openweathermap.org/img/w/${aicon}.png`);
+    })
+    .catch(error => console.error('Ошибка:', error));
+}
