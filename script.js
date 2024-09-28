902b5a90064a736a642ea871af54461a9896d60b
Tropic222 <deputat1120@gmail.com>
2024-09-28 14:24:11 +0300

    api information request

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 window.location.href = "#tab_01";
+
+const cityName = "London"; 
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Ошибка:', error));
